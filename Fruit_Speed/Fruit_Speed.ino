9bf71c138935247c5f829b70dd6e11919847ddf8
Irina <chitu.irina.nicoleta@gmail.com>
2018-12-09 14:13:34 +0200

    Work in progress

--- Fruit_Speed/Fruit_Speed.ino
+++ Fruit_Speed/Fruit_Speed.ino
@@ -6,7 +6,7 @@
 #define JOY3_X A2
 #define JOY1_BUTTON 8
 #define JOY2_BUTTON 13
-#define JOY3_BUTTON A3 //lack of pins. folosim analogii in mod digital
+#define JOY3_BUTTON A3
 #define V0_PIN 9
 
 const int DIN_PIN = 12; //data in
@@ -14,9 +14,9 @@ const int CLK_PIN = 11; //clock
 const int CS_PIN = 10; //load/cs
 
 LedControl display = LedControl(DIN_PIN, CLK_PIN, CS_PIN);
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7); //pinii la care e conectat LCD-ul
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
-const uint64_t fruits[] = {
+const uint64_t cards[] = {
   0x003c4242423c1060, //APPLE
   0x001e2222926c0000, //STRAWBERRY
   0x00e7e7e7444850e0, //BANANA
@@ -25,9 +25,13 @@ const uint64_t fruits[] = {
   0x003c664242663c00, //ORANGE
   0x3c2424242418183c, //PINEAPPLE
   0x003c424224241800, //PEAR
-  0x003c5a5a24241800  //AVOCADO
+  0x003c5a5a24241800, //AVOCADO
+  0x3c10101010141810, //1 
+  0x3c0404043c20203c, //2
+  0x3c2020283420203c, //3
+  0x2020203e24283020  //4
 };
-const int fruitLen = sizeof(fruits)/8;
+const int fruitLen = sizeof(cards)/8;
 
 const uint64_t fail = {
   0x8142241818244281 //X
@@ -38,23 +42,23 @@ const uint64_t success = {
 };
 
 int joyX[3] = {JOY1_X, JOY2_X, JOY3_X};
-int dataX[3]; //xValue, firstJoy
+int dataX[3];   //xValue, firstJoy
 int joyButton[3] = {JOY1_BUTTON, JOY2_BUTTON, JOY3_BUTTON};
-int dataButton[3]; //buttonState
+int dataButton[3];    //buttonState
 int currentPlayer;
 int nrPlayers = 1;
 int activePlayers;
 int scorePlayers[3];
-int count; //nr de "carti" de la ultima apasre corecta
-bool introduction = false; //false -> asteapta nr jucatori | true -> am terminat cu intro 
+int count;    //the number of discarded cards on the board
+bool introduction = false;    //false -> still in intro | true -> done with intro 
 int show;
 bool joyMoved = false;
-int prev = -1; //dubla
-int pprev = -1; //sandvis
-bool empty = true; //daca s a dat carte jos sau nu
+int prev = -1;    //checking for double
+int pprev = -1;   //checking for sandwich
+bool empty = true;    //if there is any card on the board
 unsigned long currentMillis;
-unsigned long previousMillis = 0; //last time "sth happened" | unsigned for time variables for time holders cause the value will quickly become too large for an int to store
-const unsigned long debounceDelay = 100; //milliseconds
+unsigned long previousMillis = 0;   //last time "sth happened" | unsigned for time variables for time holders 'cause the value will quickly become too large for an int to store
+const unsigned long debounceDelay = 100;    //milliseconds
 
 void myDelay(int ms) {
   previousMillis = millis();
@@ -63,10 +67,10 @@ void myDelay(int ms) {
 
 void readData() {
   for (int i = 0; i < nrPlayers; i++) {
-    dataX[i] = analogRead(joyX[i]);  //int firstJoy = analogRead(JOY1_X);
+    dataX[i] = analogRead(joyX[i]);   //int firstJoy = analogRead(JOY1_X);
   }
   for (int i = 0; i < nrPlayers; i++) {
-    dataButton[i] = digitalRead(joyButton[i]);  //int buttonState = digitalRead(JOY1_BUTTON);
+    dataButton[i] = digitalRead(joyButton[i]);    //int buttonState = digitalRead(JOY1_BUTTON);
   }
   currentMillis = millis();
 }
@@ -101,7 +105,7 @@ void wrong(int index, int nr) {
   lcd.print("GOTCHA!!");
   myDelay(debounceDelay * 4); //delay(400);
   lcd.clear();
-  scorePlayers[index] -= nr; //se scade punctajul jucatorului care a apasat gresit
+  scorePlayers[index] -= nr;
   char printline[20] = "Player number ";
   char conv[2];
   itoa(index + 1, conv, 10);
@@ -109,7 +113,7 @@ void wrong(int index, int nr) {
   lcd.print(printline);
   lcd.setCursor(0, 1);
   strcpy(printline, "loses ");
-  itoa(nr + 1, conv, 10); //pt ca index de la 0
+  itoa(nr + 1, conv, 10);   //because the index starts from 0 and the player's number from one
   strcat(printline, conv);
   if (nr == 0)
     strcat(printline, " point!");
@@ -118,7 +122,7 @@ void wrong(int index, int nr) {
   lcd.print(printline);
   myDelay(debounceDelay * 15); //delay(1500);
   lcd.clear();
-  displayImage(fruits[nr]); //pt ca se mai intampla sa nu dispara x-ul
+  displayImage(cards[nr]);    //to make sure the X disappears
 }
 
 void correct(int index) { 
@@ -150,8 +154,7 @@ void correct(int index) {
   display.clearDisplay(0);
 }
 
-void createPlayers() { //sa i se asocieze aici si controlerul
-  //scorePlayers = new int[nrPlayers]; //<- alocarea dinamica crapa pe arduino
+void createPlayers() {
   for (int i = 0; i < nrPlayers; i++){
     scorePlayers[i] = 20;
   }
@@ -159,7 +162,7 @@ void createPlayers() { //sa i se asocieze aici si controlerul
 }
 
 void playersTurn() {
-  //imi afiseaza cand misc pe axa Ox: ordine RANDOM-ish
+  //only Ox | RANDOM-ish order
   if ((dataX[currentPlayer] > 1000 || dataX[currentPlayer] < 100) && joyMoved == false) {
     display.clearDisplay(0);
     myDelay(debounceDelay); //delay(100);
@@ -169,9 +172,10 @@ void playersTurn() {
       pprev = prev;
       prev = show;
     }
-    currentPlayer++; //pt ca un jucator sa poata da o singura data carte (sa dea pe rand)
+    scorePlayers[currentPlayer]--;    //discards one card
+    currentPlayer++;    //wait for your turn to come again
     currentPlayer %= nrPlayers;
-    show = random(0, fruitLen); //max value is exclusive
+    show = random(0, fruitLen); 
     count++;
     joyMoved = true;
     empty = false;
@@ -184,7 +188,7 @@ void playersTurn() {
 void buttonPressed() {
   for (int i = 0; i < nrPlayers; i++) {
     if (dataButton[i] == 0) {
-      if (prev == show || pprev == show) { // dubla sau sandvis
+      if (prev == show || pprev == show) {    //double or sandwich
         correct(i);
         empty = true;
         prev = pprev = -1;
@@ -197,11 +201,11 @@ void buttonPressed() {
   }
 }
 
-void soloMode() { //schimba afisarea cu "you gain x points"
+void soloMode() {
   playersTurn();
   buttonPressed();
   if (empty == false)
-      displayImage(fruits[show]);
+      displayImage(cards[show]);
 }
 
 void finish(int index) {
@@ -212,15 +216,14 @@ void finish(int index) {
   char number[2];
   itoa(index + 1, number, 10);
   lcd.print(number);
-  display.clearDisplay(0); //stinge matricea
+  display.clearDisplay(0);
   myDelay(debounceDelay * 24); //delay(10);
-  //replay
-  //eventual intreaba daca vrea sa joace iar, iar daca nu spune bye bye
+  //add replay
   gameIntroduction();
   introduction = false;
 }
 
-void verify() { //daca e un sg jucator din start trebuie un alt tip de verificare | mai trebuie lucrat la verificare
+void verify() { 
   for (int i = 0; i < nrPlayers; i++)
     if (scorePlayers[i] == 0) {
       scorePlayers[i] = -1;
@@ -234,20 +237,19 @@ void verify() { //daca e un sg jucator din start trebuie un alt tip de verificar
 }
 
 void setup() {
-  for (int i = 0; i < 3; i++) { //nr max of players pt initializare
-    pinMode(joyButton[i], INPUT_PULLUP); // de la pullup e default high
+  for (int i = 0; i < 3; i++) {   //max number of players for setup
+    pinMode(joyButton[i], INPUT_PULLUP);    
   }
   for (int i = 0; i < 3; i++) {
-    pinMode(joyX[i], INPUT); // de la pullup e default high
+    pinMode(joyX[i], INPUT);
   }
   display.clearDisplay(0);
   display.shutdown(0, false);
-  display.setIntensity(0, 10); //cand il dadeam mai mic imi crapa pe unele fructe
+  display.setIntensity(0, 10);
 
   lcd.begin(16, 2);
   lcd.clear();
-  lcd.setCursor(1, 0);  // 2 = a cata casuta din linie sa fie aprinsa
-                     // 1 = pe a cata linie ne pozitionam; 0 = prima linie, 1 = a doua linie
+  lcd.setCursor(1, 0);
   pinMode(V0_PIN, OUTPUT); 
   analogWrite(V0_PIN, 90);
 
@@ -257,9 +259,9 @@ void setup() {
 }
 
 void loop() {
-  readData(); //joyX + buttonState + millis
+  readData(); //read: joyX + buttonState + millis
   
-  if (introduction == false) {  //introduction <- primul joystick are control (poate modifici ulterior ca primul joystick receptionat sa modifice
+  if (introduction == false) {    //introduction mode: first joystick has control over the number of players
     if (dataX[0] > 600 && joyMoved == false) {
       nrPlayers++;
       if (nrPlayers > 3)
@@ -289,16 +291,16 @@ void loop() {
       createPlayers();
     }
   }
-  else {  //in joc
-    playersTurn(); //se da carte jos
-    buttonPressed(); //se verifica daca se apasa butonul sau nu
+  else {    //game mode:
+    playersTurn();
+    buttonPressed();    //check whether or not a button was pressed ! => attachInterrupt !
     
-    if (empty == false) { //daca nu e gol stackul de carti se afiseaza cartea, altfel se asteapta sa se dea din cursor
-      displayImage(fruits[show]);
+    if (empty == false) {   //if the stack is empty wait for a player to move the joystick, else keep showing the current card
+      displayImage(cards[show]);
     }
 
     if (nrPlayers != 1)
-      verify(); //se verifica daca avem un castigator
+      verify();   //check for winner
     else {
       lcd.setCursor(1, 0);
       lcd.print("Current Score:");
@@ -306,6 +308,7 @@ void loop() {
       lcd.print(scorePlayers[0]);
     }
   }
+  //for now the only place where you can see the scores is in the serial monitor
   Serial.println("Score:");
   Serial.println(scorePlayers[0]);
   Serial.println(scorePlayers[1]);
